707a261a98a22e3216847ef48060a7bfd8bbdc5e
satoshionoda <satoshionoda@gmail.com>
2022-01-31 14:51:00 +0900

    update fc

--- index.mjs
+++ index.mjs
@@ -2,24 +2,28 @@ import cac from "cac";
 import { readFileSync, writeFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import { camelCase, pascalCase } from "change-case";
 
 const cli = cac();
 
 const execute = () => {
   cli.command("fc <baseName>", "create react functional component").action((baseName) => {
-    replace("fc.txt", `${baseName}.tsx`, baseName);
-    replace("sb.txt", `${baseName}.stories.tsx`, baseName);
+    replace("fc.txt", baseName, `.tsx`);
+    replace("sb.txt", baseName, `.stories.tsx`);
   });
   cli.help();
   cli.parse();
 };
 
-const replace = (templateName, fileName, baseName) => {
+const replace = (templateName, baseName, extention) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const templatePath = join(__dirname, "templates", templateName);
   const data = readFileSync(templatePath, "utf8");
-  const replaced = data.replace(/__placeholder__/g, baseName);
+  const replaced = data
+    .replace(/__PlaceHolder__/g, pascalCase(baseName))
+    .replace(/__placeHolder__/g, camelCase(baseName));
+  const fileName = pascalCase(baseName) + extention;
   const resultPath = join(process.cwd(), fileName);
   writeFileSync(resultPath, replaced);
   console.log("created:", fileName);
